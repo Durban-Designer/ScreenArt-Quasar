db0261efb2aca89322addf3ebfbd861054e4dd0b
Spiralis-Solutions <krisstjohn96@gmail.com>
2018-01-02 16:27:22 -0700

    stuff

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,11 +1,18 @@
 <template>
-  <div class="slidebox">
-    <transition name="slide"><img class="slide1" v-if="show === 0" src="../assets/Seasons.jpg"></transition>
-    <transition name="slide"><img class="slide2" v-if="show === 1" src="../assets/birds.jpg"></transition>
-    <transition name="slide"><img class="slide3" v-if="show === 2" src="../assets/angles.jpg"></transition>
-    <transition name="slide"><img class="slide4" v-if="show === 3" src="../assets/grate.jpg"></transition>
-    <transition name="slide"><img class="slide5" v-if="show === 4" src="../assets/humans.jpg"></transition>
-    <transition name="slide"><img class="slide6" v-if="show === 5" src="../assets/window.jpg"></transition>
+  <div class="main">
+    <div class="slidebox">
+      <transition name="slide"><img class="slide1" v-if="show === 0" src="../assets/Seasons.jpg"></transition>
+      <transition name="slide"><img class="slide2" v-if="show === 1" src="../assets/birds.jpg"></transition>
+      <transition name="slide"><img class="slide3" v-if="show === 2" src="../assets/angles.jpg"></transition>
+      <transition name="slide"><img class="slide4" v-if="show === 3" src="../assets/grate.jpg"></transition>
+      <transition name="slide"><img class="slide5" v-if="show === 4" src="../assets/humans.jpg"></transition>
+      <transition name="slide"><img class="slide6" v-if="show === 5" src="../assets/window.jpg"></transition>
+    </div>
+    <div class="navbar">
+      <button class="Work">Work</button>
+      <button class="Contact">Contact</button>
+      <button class="Team">Team</button>
+    </div>
   </div>
 </template>
 
@@ -45,8 +52,65 @@ export default {
 </script>
 
 <style scoped lang='less'>
+@base-font:'Montserrat', sans-serif;
 
+.main {
+  height: 3000px;
+}
+.navbar {
+  width: 100%;
+  height: 60px;
+  background:rgba(0,0,0,0.6);
+  position: absolute;
+  z-index: 7;
+  text-align: center;
+  line-height: 70px;
+  font-family: @base-font;
+  font-weight: lighter;
+}
+.Work:hover {
+  transform:scale(1.3,1.3);
+  -webkit-transform:scale(1.3,1.3);
+  -moz-transform:scale(1.3,1.3);
+}
+.Work {
+  background-color: transparent;
+  border: none;
+  color: #fff;
+  font-family: @base-font;
+  font-size: 1.4em;
+  width: 25%;
+}
 
+.Contact:hover {
+  transform:scale(1.3,1.3);
+  -webkit-transform:scale(1.3,1.3);
+  -moz-transform:scale(1.3,1.3);
+}
+
+.Contact {
+  background-color: transparent;
+  border: none;
+  color: #fff;
+  font-family: @base-font;
+  font-size: 1.4em;
+  width: 25%;
+}
+
+.Team:hover {
+  transform:scale(1.3,1.3);
+  -webkit-transform:scale(1.3,1.3);
+  -moz-transform:scale(1.3,1.3);
+}
+
+.Team {
+  background-color: transparent;
+  border: none;
+  color: #fff;
+  font-family: @base-font;
+  font-size: 1.4em;
+  width: 25%;
+}
 .slide1 {
   position: absolute;
   width: 100%;
