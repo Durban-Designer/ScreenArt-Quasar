5390f13922a977553e2d190255b0456d306f1d31
Spiralis-Solutions <krisstjohn96@gmail.com>
2018-01-02 14:29:22 -0700

    herysago

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,15 +1,37 @@
 <template>
-  <div></div>
+  <div>
+    <transition name="slide">
+      <img class="slide1" v-if="show" src="../assets/bluestar.jpg">
+    </transition>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'home',
   data () {
-    return {}
+    return {
+      show: true
+    }
   }
 }
+function toggleShow () {
+  this.show = !this.show
+  console.log(this.show)
+}
+setInterval(toggleShow, 6000)
 </script>
 
 <style scoped lang='less'>
+
+.slide1 {
+  width: 100%;
+}
+.slide-enter-active, .slide-leave-active {
+  transition: all .3s ease;
+}
+.slide-enter, .slide-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  transform: translateX(10px);
+  opacity: 0;
+}
 </style>
