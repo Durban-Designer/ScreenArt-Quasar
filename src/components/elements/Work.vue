f4eda19ad4567797d40015ae9c6257e549d55bee
Spiralis-Solutions <krisstjohn96@gmail.com>
2018-01-08 13:22:43 -0700

    ech

--- src/components/elements/Work.vue
+++ src/components/elements/Work.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="workPage">
     <h1>Currently Under Construction</h1>
+    <div class="bart"></div>
   </div>
 </template>
 
 <script>
 export default {
+  name: 'Work',
   data () {
     return {}
   }
@@ -13,7 +15,27 @@ export default {
 </script>
 
 <style scoped lang='less'>
+@base-font:'Montserrat', sans-serif;
 
 .workPage {
+  margin-top: 105px;
+  width: 100%;
+  height: 300px;
+  z-index: 10;
+  position: absolute;
+  background:rgba(0,0,0,0.6);
+
+}
+h1 {
+  font-family: @base-font;
+  font-weight: lighter;
+  font-size: 2em;
+  color: #fff;
+  text-shadow: 2px 2px 3px black;
+  text-align: center;
+  line-height: 100px;
+}
+.bart {
+  background-image: url("../assets/Bart.svg");
 }
 </style>
