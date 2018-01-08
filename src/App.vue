02636be420efc4991cd58ccb24bf3e8ff281c2a4
Spiralis-Solutions <krisstjohn96@gmail.com>
2018-01-08 10:23:52 -0700

    redo

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,14 @@
   <!-- Don't drop "q-app" class -->
   <div id="q-app">
     <router-view />
-    <app-header></app-header>
   </div>
 </template>
 
 <script>
-import Header from './Header.vue'
-export default {
-  components: {
-    'app-header': Header
-  }
-}
+/*
+ * Root component
+ */
+export default {}
 </script>
 
 <style></style>
