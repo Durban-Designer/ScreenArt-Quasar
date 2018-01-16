6f502d590d16953cee179db6d5be170993ccaa9e
Grapetoast <bonham.goodman@gmail.com>
2018-01-16 15:40:01 -0700

    .

--- src/components/Inbox.vue
+++ src/components/Inbox.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="main">
+    <input type="text" class="search" placeholder="Search Messages" v-model="searchBox"></input>
+    <select class="filter">
+      <option value=""></option>
+      <option value=""></option>
+      <option value=""></option>
+      <option value=""></option>
+    </select>
+    <button type="submit" class="searchButton" v-on:click="search">SEARCH</button>
     <div class="messageList" v-on:click="displayMessage" v-for="message in messages">
       <h5 v-on:click="messageItemDisplay(message)" class="message">{{message.name}}:{{message.status}}</h5>
     </div>
